0db3e3fd36aafb0554d1deb6e5a27adfa274fdc3
JoopJ <jppnhmr@gmail.com>
2025-02-05 19:17:22 +0000

    Updated NewConnection variable name
    Updated NewConnection window class to use _ui_components instead of ui_components

--- src/ui/windows/new_connection_window.py
+++ src/ui/windows/new_connection_window.py
@@ -11,12 +11,12 @@ class NewConnectionWindow(tk.Toplevel):
     connect to the server or close the window.
     
     Args:
-        ui_components (UIComponents): the UIComponents singleton
+        _ui_components (UIComponents): the UIComponents singleton
     """
-    def __init__(self, ui_components: UIComponents):
+    def __init__(self, _ui_components: UIComponents):
         tk.Toplevel.__init__(self)
         
-        self.ui_components = ui_components
+        self._ui_components = _ui_components
         
         self.title("New Connection")
         self.geometry("340x140")
@@ -32,13 +32,13 @@ class NewConnectionWindow(tk.Toplevel):
         Create Close button to close this window.
         """
         # Host, user, and password entries
-        self.host_entry = self.ui_components.create_entry(self, 30)
-        self.user_entry = self.ui_components.create_entry(self, 30)
-        self.password_entry = self.ui_components.create_entry(self, 30, "*")
+        self.host_entry = self._ui_components.create_entry(self, 30)
+        self.user_entry = self._ui_components.create_entry(self, 30)
+        self.password_entry = self._ui_components.create_entry(self, 30, "*")
         # Labels for host, user, and password
-        label_host = self.ui_components.create_label(self, "Host")
-        label_user = self.ui_components.create_label(self, "User")
-        label_password = self.ui_components.create_label(self, "Password")
+        label_host = self._ui_components.create_label(self, "Host")
+        label_user = self._ui_components.create_label(self, "User")
+        label_password = self._ui_components.create_label(self, "Password")
         
         # Grid layout
         label_host.grid(row=0, column=0, padx=1, pady=pady)
@@ -49,14 +49,14 @@ class NewConnectionWindow(tk.Toplevel):
         self.password_entry.grid(row=2, column=1, padx=1, pady=pady)
         
         # Connect to server button
-        self.connect_btn = self.ui_components.create_button(
+        self.connect_btn = self._ui_components.create_button(
             parent = self,
             text = "Connect",
             command = self._connect_server,
             width = 20)
         # Close window button
         self.connect_btn.grid(row=3, column=1, padx=5, pady=10, sticky=tk.E)
-        self.close_btn = self.ui_components.create_button(
+        self.close_btn = self._ui_components.create_button(
             parent = self,
             text = "close",
             event = "CLOSE_WINDOW",
@@ -65,13 +65,13 @@ class NewConnectionWindow(tk.Toplevel):
         self.close_btn.grid(row=3, column=0, padx=5, pady=10, sticky=tk.E)
     
     def _subscribe_events(self):
-        self.ui_components.subscribe(
+        self._ui_components.subscribe(
             "CONNECT_SERVER_FAIL", 
             self._connect_fail)
     
     # -------------------------- Event Callbacks -------------------------- #            
     def _connect_server(self):
-        self.ui_components.publish(
+        self._ui_components.publish(
             "CONNECT_TO_SERVER", 
             {
                 "host": self.host_entry.get(),
@@ -79,9 +79,13 @@ class NewConnectionWindow(tk.Toplevel):
                 "password": self.password_entry.get()
             }
         )
+        self._ui_components.publish(
+            "CLOSE_WINDOW",
+            {"window_name": "new_connection"}
+        )
     
     def _connect_fail(self):
-        self.ui_components.create_message_box(
+        self._ui_components.create_message_box(
             "Connection Failed",
             "Failed to connect to the server. Please check your credentials and try again."
         )        
